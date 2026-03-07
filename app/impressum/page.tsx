0cfee7dcf4b483dca6ee58d22243a559b51bbe8a
Jesse Capizzi <capizzijesse@gmail.com>
2026-03-07 20:43:10 +0100

    fix impressum page

---
+++ app/impressum/page.tsx
@@ -0,0 +1,27 @@
+export default function Page() {
+  return (
+    <div style={{ padding: "40px", maxWidth: "800px", margin: "0 auto" }}>
+      <h1>Impressum</h1>
+
+      <p>
+        Makler Plattform
+      </p>
+
+      <p>
+        Verantwortlich für den Inhalt dieser Website:
+      </p>
+
+      <p>
+        Jesse Capizzi
+      </p>
+
+      <p>
+        Schweiz
+      </p>
+
+      <p>
+        E-Mail: info@makler-ai.ch
+      </p>
+    </div>
+  );
+}
