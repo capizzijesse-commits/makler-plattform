d7ee76dbea0937e35037fc4a8c35f12bc77e0b34
Jesse Capizzi <capizzijesse@gmail.com>
2026-03-24 20:32:26 +0100

    fix login clean version

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,27 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  try {
-    const { email, password } = await req.json();
+  const { email } = await req.json();
 
-    // HARDCODE LOGIN (für Launch)
-    if (email === "test@test.com" && password === "123456") {
-      return NextResponse.json({
-        user: {
-          email,
-          name: "Test User",
-        },
-      });
-    }
-
-    return NextResponse.json(
-      { error: "Falsche Login Daten" },
-      { status: 401 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Serverfehler" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    user: {
+      name: email.split("@")[0],
+      email,
+    },
+  });
 }
