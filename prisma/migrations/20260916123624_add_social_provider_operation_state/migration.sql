8caa7ba89607b3cd1d724e7f6d288caeac00efd0
Jesse Capizzi <capizzijesse@gmail.com>
2026-09-16 12:44:15 +0200

    fix: remove BOM from provider operation migration

--- prisma/migrations/20260916123624_add_social_provider_operation_state/migration.sql
+++
@@ -1,8 +0,0 @@
-﻿ALTER TABLE "SocialPublishJob"
-ADD COLUMN "providerOperationId" TEXT,
-ADD COLUMN "providerOperationType" TEXT,
-ADD COLUMN "providerOperationState" TEXT,
-ADD COLUMN "providerOperationUpdatedAt" TIMESTAMP(3);
-
-CREATE INDEX "SocialPublishJob_provider_providerOperationState_idx"
-ON "SocialPublishJob"("provider", "providerOperationState");
